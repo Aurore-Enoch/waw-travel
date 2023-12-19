16ba22935d13d46fa8b2be0bf2ee9b38d5ebd024
Laanaoui Abderrazzaq <laanaoui.e2305353@etud.univ-ubs.fr>
2023-12-19 22:16:20 +0100

    auth pages UI

--- templates/_footer.php
+++ templates/_footer.php
@@ -1,3 +1,8 @@
-<div class="container">
-<footer><p>Waw.travel &copy; 2023 - Tout droits réservés</p></footer>
+<div class="app-footer">
+    <footer>
+        <h2>waw.travel &copy; 2023 - Tout droits réservés</h2>
+        <a href="?path=/">Home</a> | 
+        <a href="#top">Top</a> | 
+        <a href="?path=/about">About Us</a>
+    </footer>
 </div>

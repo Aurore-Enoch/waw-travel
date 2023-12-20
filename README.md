13e21c4cd581006bd68855b289adbfcba1aead2f
Laanaoui Abderrazzaq <laanaoui.e2305353@etud.univ-ubs.fr>
2023-12-20 03:58:27 +0100

    fixing Flash messages + adding more error messgaes + improving security

--- README.md
+++ README.md
@@ -1,10 +1,25 @@
 # waw.travel
 Réaliser une plateforme permettant aux utilisateurs inscrits de créer un carnet de voyage (type road trip avec plusieurs étapes). Pour pouvoir être consultée facilement par la famille des voyageurs, une page web publique détaillera l'ensemble du road trip.
 
-## Installation
+## Requirements
+- Docker
 
+## Installation
 1. Build the Docker image: `docker build -t wawtravel .`
 2. Run the Docker container: `docker run -p 8000:8000 wawtravel`
-3. Create the database: `docker exec <container_id> mysql -u root -e "CREATE DATABASE waw_travel;"`
-4. Run the SQL script: `docker exec  <container_id> mysql -u root waw_travel < db/waw_travel.sql`
+
+## Database setup
+wait for the container to be up and running, then:
+1. Create the database: `docker exec -i <container_id> mysql -u root -e "CREATE DATABASE waw_travel;"`
+2. Run the SQL script: `docker exec -i  <container_id> mysql -u root waw_travel < db/waw_travel.sql`
 > Note: To get the container id, run `docker ps` and use the first 3 characters of the id.
+
+## Usage
+1. Go to http://localhost/waw-travel/public/index.php?path=/inscription
+2. Create an account
+
+## Authors
+- [Achraf Ait Mbarek](https://github.com/achrafaitmbarek)
+- [Aurore Enoch](https://github.com/Aurore-Enoch)
+- [Liticia Tadjer](https://github.com/ltadjer)
+
--- src/Entity/RoadTrip.php
+++ src/Entity/RoadTrip.php
@@ -6,40 +6,49 @@ use App\Entity\User;
 use App\Manager\CarTypeManager;
 
 #[\AllowDynamicProperties]
-class RoadTrip {
+class RoadTrip
+{
 
-	private ?int $id;
-	private ?string $title;
+    private ?int $id;
+    private ?string $title;
     private ?CarType $carType;
     private ?User $user;
-    
-	public function getId(): ?int {
-		return $this->id;
-	}
 
-	public function getTitle(): ?string {
-		return $this->title;
-	}
-	public function setTitle(?string $title): void {
-		$this->title = $title;
-	}
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
 
-    public function getCarType(): ?CarType {
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+    public function setTitle(?string $title): void
+    {
+        $this->title = $title;
+    }
+
+    public function getCarType(): ?CarType
+    {
         return $this->carType;
     }
-    public function setCarType(?CarType $carType): void {
+    public function setCarType(?CarType $carType): void
+    {
         $this->carType = $carType;
     }
 
-    public function getUser(): ?User {
+    public function getUser(): ?User
+    {
         return $this->user;
-    }  
-    public function setUser(?User $user): void {
+    }
+    public function setUser(?User $user): void
+    {
         $this->user = $user;
     }
 
-    public function carTypeId(): ?int {
-        return $this->car_type_id;
+    public function carTypeId(): ?int
+    {
+        return $this->carType->getId();
     }
 
 }

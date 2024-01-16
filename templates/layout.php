3ce789c86e5c14f556583dcd7a8f3d2748f84891
Aurore <aurore.enoch@gmail.com>
2024-01-16 16:31:00 +0100

    merge Front-End

--- templates/layout.php
+++ templates/layout.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/app.css"/>
     <link rel="icon" href="img/logo-waw-travel.png"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"/>
     <title>
 		<?php if (isset($data['seo']['title'])) echo $data['seo']['title'] . ' - '; ?>
 		Waw Travel
@@ -15,6 +16,17 @@
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="bg-[#FEF3ED]">
+    <script>
+    document.addEventListener('DOMContentLoaded', (event) => {
+      const button = document.querySelector('[data-collapse-toggle="navbar-default"]');
+      const navbar = document.getElementById('navbar-default');
+
+      button.addEventListener('click', () => {
+        navbar.classList.toggle('hidden');
+        navbar.classList.toggle('block');
+      });
+    });
+    </script>
     <?php include '_header.php'; ?>
     <main>
         <?php require $templatePath ?>
